6be5716afdc6ab20e5b58835d8ec103299a83754
Jakub Smolik <smolikja@protonmail.com>
2025-04-22 11:49:44 +0200

    feat: email reporting improvements

--- lib/src/email_reporting/email_reporting.dart
+++ lib/src/email_reporting/email_reporting.dart
@@ -3,12 +3,29 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
+import 'package:logging/logging.dart';
 import 'package:mailto/mailto.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+/// Class for reporting errors via email
 class EmailReporting {
+  /// Initialize the EmailReporting with optional configuration
+  ///
+  /// [appFlavor] - The build flavor/scheme of the app (e.g., 'dev', 'prod')
+  /// [logger] - Optional logger for logging events
+  static void initialize({
+    String? appFlavor,
+    Logger? logger,
+  }) {
+    _appFlavor = appFlavor;
+    _logger = logger ?? Logger('EmailReporting');
+  }
+
+  // Static configuration
+  static String? _appFlavor;
+  static Logger _logger = Logger('EmailReporting');
+
   /// Date and time of reporting
   static String get _dateTime => DateTime.now().toIso8601String();
 
@@ -25,7 +42,7 @@ class EmailReporting {
       (await PackageInfo.fromPlatform()).buildNumber;
 
   /// App build scheme
-  static String get _appBuildScheme => appFlavor ?? 'unknown';
+  static String get _appBuildScheme => _appFlavor ?? 'unknown';
 
   /// User ID
   static String get _userID =>
@@ -80,49 +97,81 @@ class EmailReporting {
   static Future<String> get _battery async =>
       (await Battery().batteryLevel).toString();
 
+  /// Compose the email body with all relevant information
   static Future<String> _composeBody(Object error, StackTrace stack) async {
     return '''
       date time: $_dateTime
-      
+
       app ID: ${await _appID}
       app version: ${await _appVersion}
       app build: ${await _appBuild}
       app build scheme: $_appBuildScheme
-      
+
       user ID: $_userID
       platform: ${await _platform}
       device: ${await _device}
       device OS: ${await _deviceOS}
       all device info: ${await _deviceInfo}
-      
+
       connectivity: ${await _connectivity}
       battery: ${await _battery}
-      
+
       error: $error
-      
+
       stack trace: $stack
       ''';
   }
 
+  /// Launch a URL
   static Future<void> _launchUrl(Uri url) async {
-    if (!await launchUrl(url)) {
-      throw Exception('Could not launch $url');
+    try {
+      if (!await launchUrl(url)) {
+        throw Exception('Could not launch $url');
+      }
+    } catch (e) {
+      _logger.severe('Error launching URL: $url', e);
+      rethrow;
     }
   }
 
+  /// Compose and send an error report via email
+  ///
+  /// [error] - The error object to report
+  /// [stack] - The stack trace associated with the error
+  /// [to] - List of recipient email addresses
+  /// [cc] - Optional list of CC email addresses
+  /// [appFlavor] - Optional app flavor to use for this specific report
   static Future<void> composeAnErrorEmail({
     required Object error,
     required StackTrace stack,
     required List<String> to,
     List<String>? cc,
+    String? appFlavor,
   }) async {
-    final mailtoLink = Mailto(
-      to: to,
-      cc: cc,
-      subject: 'Application error reporting',
-      body: await _composeBody(error, stack),
-    );
+    // Use provided appFlavor for this report if specified
+    final previousFlavor = _appFlavor;
+    if (appFlavor != null) {
+      _appFlavor = appFlavor;
+    }
 
-    await _launchUrl(Uri.parse(mailtoLink.toString()));
+    try {
+      final mailtoLink = Mailto(
+        to: to,
+        cc: cc,
+        subject: 'Application error reporting',
+        body: await _composeBody(error, stack),
+      );
+
+      await _launchUrl(Uri.parse(mailtoLink.toString()));
+      _logger.info('Error email composed and launched');
+    } catch (e, st) {
+      _logger.severe('Failed to compose error email', e, st);
+      rethrow;
+    } finally {
+      // Restore previous flavor
+      if (appFlavor != null) {
+        _appFlavor = previousFlavor;
+      }
+    }
   }
 }
